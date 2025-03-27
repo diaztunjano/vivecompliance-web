0eac776286f5c135a13df86656ae5bd3988b470c
David Díaz <david@inventures.cl>
2025-03-27 11:16:03 -0300

    feat: add light theme by default

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 import "./index.css";
+import { inject } from '@vercel/analytics';
+
+inject();
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
